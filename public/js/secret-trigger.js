c04f6f0d5a532012d2cca69b7bfa938140818b29
Neil100o <122431350+Neil100o@users.noreply.github.com>
2026-07-12 15:14:59 +0800

    合成大74

--- public/js/secret-trigger.js
+++ public/js/secret-trigger.js
@@ -3,7 +3,6 @@
   var PET_VIDEO_SRC = '/pet.webm';
   var PET_WIDTH = 560;
   var PET_HEIGHT = 420;
-  var PET_HEIGHT = 210;
   var SPEED_MIN = 0.12;
   var SPEED_MAX = 0.35;
   var EDGE_MARGIN = 40;
